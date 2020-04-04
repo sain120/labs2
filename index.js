ffedf3369d559e775c7c2f72f05d47cf083ff2dc
sain120 <alejandro.camacho.arias@gmail.com>
2020-04-04 16:53:37 +0000

    final version l04

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cool = require("cool-ascii-faces");
 
 const express = require("express");
 
+var app = express();
+
 app.get("/cool", (request, response) => {
 	response.send("<html>"+cool()+"</html>");
 })
